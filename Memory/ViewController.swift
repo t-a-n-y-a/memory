2c991c306d0c8790144b4b60be73885c3ba30b6b
Tanya Sahin <tanya@Tanyas-MBP.localdomain>
2014-11-29 12:56:14 +0100

    fixed new game bug, added new game button, completion time displayed fixed

--- Memory/ViewController.swift
+++ Memory/ViewController.swift
@@ -14,12 +14,13 @@ class ViewController: UIViewController {
     var openedCardIndex : Int?
     var nPairsFound = 0
     var moves = 0
-    var timestamp = NSDate()
+    var timestamp : NSDate?
     var firstCard = true
     
     @IBOutlet var buttons: [UIButton]!
     @IBOutlet weak var movesLabel: UILabel!
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let names = ["Apple", "Car", "Dog", "Flower", "Star", "Tree"]
@@ -38,12 +39,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func newGameButton(sender: AnyObject) {
+        newGame()
+    }
 
     @IBAction func buttonSelect(sender: UIButton) {
         assert(sender.tag < cards.count, "Fewer cards than buttons")
         if sender.tag < cards.count && openedCardIndex != sender.tag && cards[sender.tag].matched == false {
             if firstCard {
                 timestamp = NSDate()
+                firstCard = false
             }
             
             let card = cards[sender.tag]
@@ -64,11 +70,17 @@ class ViewController: UIViewController {
                 openedCardIndex = sender.tag
             } else { //two cards open
                 if card == cards[openedCardIndex!] {
-                    let completionTime = timestamp.timeIntervalSinceNow //NOT DONE YET; nice improvement: display completion time while game is running
                     card.matched = true
                     nPairsFound++
                     if nPairsFound == cards.count/2 {
-                        let winningAlert = UIAlertController(title: "Congratulations", message: "You won. Moves: \(moves) Completion Time: \(completionTime)", preferredStyle: UIAlertControllerStyle.Alert)
+                        var message = "You won. Moves: \(moves)"
+
+                        if timestamp != nil {
+                            let completionTime = Int(timestamp!.timeIntervalSinceNow*(-1))
+                            message += " Completion Time: \(completionTime) seconds"
+                        }
+                        
+                        let winningAlert = UIAlertController(title: "Congratulations", message: message, preferredStyle: UIAlertControllerStyle.Alert)
                         winningAlert.addAction(UIAlertAction(title: "New Game", style: UIAlertActionStyle.Default, handler: { action in
                             self.newGame()
                             }))
@@ -100,11 +112,13 @@ class ViewController: UIViewController {
     func newGame () {
         for (var i = 0; i < cards.count; i++) {
             buttons[i].setBackgroundImage(UIImage(named: "CardBackground"), forState: UIControlState.Normal)
+            cards[i].matched = false
         }
         shuffle()
         moves = 0
         movesLabel.text = String(0)
         nPairsFound = 0
+        firstCard = true
     }
 }
 
